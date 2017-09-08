5146a9360192e025e01784c386ed5303f0b42f79
lukePeavey <1williampeavey@gmail.com>
2017-09-08 01:38:32 -0400

    Add todo item for renaming key component

--- src/components/keypad/Keypad.js
+++ src/components/keypad/Keypad.js
@@ -6,6 +6,8 @@ const Keypad = ({ keys: keypadRows, handleClick }) => (
     {keypadRows.map((keys, index) => (
       <div className="Keypad-row" key={`row${index}`}>
         {keys.map(key => {
+          // @todo - Rename keys and keypad something else.
+          // Just noticed this is very confusing with React's key prop.
           return <Key onClick={event => {}} key={key.id} {...key} />
         })}
       </div>
