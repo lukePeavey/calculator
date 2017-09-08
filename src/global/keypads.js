07f903067a178d03934809e0cfbaf741a14f61b4
lukePeavey <1williampeavey@gmail.com>
2017-09-08 00:58:59 -0400

    Define keypad constants

---
+++ src/global/keypads.js
@@ -0,0 +1,16 @@
+import * as key from './keys'
+// This determines the layout of keys on the keypad.
+const keypads = {
+
+  // Basic keypad
+  basic: [
+    [key.clear, key.negate, key.percent, key.divide],
+    [key.seven, key.eight, key.nine, key.multiply],
+    [key.four, key.five, key.six, key.subtract],
+    [key.one, key.two, key.three, key.add],
+    [key.zero, key.decimal, key.equals]
+  ]
+
+  // @todo Add scientific keypad
+}
+export default keypads
---
+++ src/global/keys.js
@@ -0,0 +1,30 @@
+// Function Keys
+export const clear =  { type: 'clear', label: 'C', id: 'clear' }
+export const equals = { type: 'equals', label: '&equals;', id: 'equals' }
+export const del = { type: 'del', label: '\u232b', id: 'delete' }
+
+// Number Keys
+export const one =   { type: 'number', label: '1',  id: 'one' }
+export const two =   { type: 'number', label: '2',  id: 'two' }
+export const three = { type: 'number', label: '3',  id: 'three' }
+export const four =  { type: 'number', label: '4',  id: 'four' }
+export const five =  { type: 'number', label: '5',  id: 'five' }
+export const six =   { type: 'number', label: '6',  id: 'six' }
+export const seven = { type: 'number', label: '7',  id: 'seven' }
+export const eight = { type: 'number', label: '8',  id: 'eight' }
+export const nine =  { type: 'number', label: '9',  id: 'nine' }
+export const zero =  { type: 'number', label: '0',  id: 'zero' }
+export const decimal = { type: 'number', label: '.',  id: 'decimal' }
+
+// Binary Operation Keys
+export const add =  { type: 'binaryOperation',  abel: '&plus;', id: 'plus' }
+export const subtract = { type: 'binaryOperation', label: '&minus;', id: 'minus' }
+export const multiply = { type: 'binaryOperation', label: '&times;', id: 'times' }
+export const divide = { type: 'binaryOperation', label: '&divide;', id: 'div' }
+export const pow = { type: 'binaryOperation', label: 'x<sup>y</sup>', id: 'pow' }
+
+// Unary Operation Keys
+export const negate = { type: 'unaryOperation', label: '&plus;/&minus;', id: 'negate' }
+export const percent = { type: 'unaryOperation', label: '%', id: 'percent' }
+
+// @todo Add keys for scientific calculator
