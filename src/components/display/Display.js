b42c43f380b2cc772b160889eb934b27553ae78c
lukePeavey <1williampeavey@gmail.com>
2017-09-09 02:50:03 -0400

    Scale font size for long display values

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Display.css'
 
-const Display = ({ value }) => (
-  <section className="Display">
-    <div className="Display-value">{value}</div>
-  </section>
-)
+const Display = ({ value = '0' }) => {
+  // Adjusts fontsize based on the length of the display value, so the
+  // value fits in the display area. Might not work consistently in
+  // all browsers/devices.
+  let overflow = String(value).slice(12).length
+  let fontSize = `${100 - overflow * (overflow < 11 ? 5 : 4)}%`
+  return (
+    <section className="Display">
+      <div className="Display-value" style={{ fontSize }}>
+        {value}
+      </div>
+    </section>
+  )
+}
 
 export default Display
