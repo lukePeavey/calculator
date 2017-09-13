15da193f8092313532cd26212914fdefed170219
lukePeavey <1williampeavey@gmail.com>
2017-09-13 14:41:49 -0400

    Add the scientific keypad

--- src/global/keypads.js
+++ src/global/keypads.js
@@ -9,6 +9,63 @@ const keypads = {
     [key.four, key.five, key.six, key.subtract],
     [key.one, key.two, key.three, key.add],
     [key.zero, key.decimal, key.equals]
+  ],
+  scientific: [
+    [
+      key.openParenthesis,
+      key.closeParenthesis,
+      key.memoryClear,
+      key.memoryAdd,
+      key.memoryRemove,
+      key.memoryRecall,
+      key.clear,
+      key.negate,
+      key.percent,
+      key.divide
+    ],[
+      key.alt,
+      key.square,
+      key.cube,
+      key.toPower,
+      key.eToXPower,
+      key.tenToXPower,
+      key.seven,
+      key.eight,
+      key.nine,
+      key.multiply
+    ], [
+      key.inverse,
+      key.squareRoot,
+      key.cubeRoot,
+      key.nthRoot,
+      key.naturalLogarithm,
+      key.logarithm,
+      key.four,
+      key.five,
+      key.six,
+      key.subtract
+    ], [
+      key.factorial,
+      key.sine,
+      key.cosine,
+      key.tangent,
+      key.e,
+      key.exponential,
+      key.one,
+      key.two,
+      key.three,
+      key.add,
+    ], [
+      key.trigUnit,
+      key.sineH,
+      key.cosineH,
+      key.tangentH,
+      key.pi,
+      key.rand,
+      key.zero,
+      key.decimal,
+      key.equals
+    ]
   ]
 }
 export default keypads
--- src/global/keys.js
+++ src/global/keys.js
@@ -2,6 +2,14 @@
 -----------------------------------------------*/
 export const clear =  { type: 'clear', label: 'C', id: 'clear' }
 export const equals = { type: 'equals', label: '&equals;', id: 'equals' }
+export const openParenthesis = { type: 'function', label: '(', id: 'openParenthesis' }
+export const closeParenthesis = { type: 'function', label: ')', id: 'closeParenthesis' }
+export const memoryClear = { type: 'function', label: 'mc', id: 'memoryClear' }
+export const memoryAdd = { type: 'function', label: 'm&plus;', id: 'memoryAdd' }
+export const memoryRemove = { type: 'function', label: 'm&plus;', id: 'memoryRemove' }
+export const memoryRecall = { type: 'function', label: 'mr', id: 'memoryRecall' }
+export const alt = { type: 'function', label: '2<sup>nd</sup>', id: 'alt' }
+export const trigUnit = { type: 'function', label: 'Rad', id: 'trigUnit' }
 
 /* Number Keys
 -----------------------------------------------*/
@@ -23,8 +31,33 @@ export const add =  { type: 'binaryOperation',  label: '&plus;', id: 'plus' }
 export const subtract = { type: 'binaryOperation', label: '&minus;', id: 'minus' }
 export const multiply = { type: 'binaryOperation', label: '&times;', id: 'times' }
 export const divide = { type: 'binaryOperation', label: '&divide;', id: 'div' }
+export const toPower = { type: 'binaryOperation', label: 'x<sup>y</sup>', id: 'pow' }
+export const nthRoot = { type: 'binaryOperation', label: '<sup>y</sup>&radic;<sub>x</sub>', id: 'nthRoot' }
 
 /* Unary Operation Keys
 -----------------------------------------------*/
 export const negate = { type: 'unaryOperation', label: '&plus;/&minus;', id: 'neg' }
 export const percent = { type: 'unaryOperation', label: '&#37', id: 'percent' }
+export const square = { type: 'unaryOperation', label: 'x<sup>2</sup>', id: 'square' }
+export const cube = { type: 'unaryOperation', label: 'x<sup>3</sup>', id: 'cube' }
+export const eToXPower = { type: 'unaryOperation', label: 'e<sup>x</sup>', id: 'eToXPower' }
+export const tenToXPower = { type: 'unaryOperation', label: '10<sup>x</sup>', id: 'tenToXPower' }
+export const inverse = { type: 'unaryOperation', label: '1/x', id: 'inverse' }
+export const squareRoot = { type: 'unaryOperation', label: '<sup>2</sup>&radic;<sub>x</sub>', id: 'sqrt' }
+export const cubeRoot = { type: 'unaryOperation', label: '<sup>3</sup>&radic;<sub>x</sub>', id: 'cbrt' }
+export const naturalLogarithm = { type: 'unaryOperation', label: 'ln', id: 'ln' }
+export const logarithm = { type: 'unaryOperation', label: 'log', id: 'log' }
+export const exponential = { type: 'unaryOperation', label: 'EE', id: 'toExponential' }
+export const factorial = { type: 'unaryOperation', label: 'x!', id: 'factorial' }
+export const sine = { type: 'unaryOperation', label: 'sin', id: 'sin' }
+export const cosine = { type: 'unaryOperation', label: 'cos', id: 'cos' }
+export const tangent = { type: 'unaryOperation', label: 'tan', id: 'tan' }
+export const sineH = { type: 'unaryOperation', label: 'sinh', id: 'sinh' }
+export const cosineH = { type: 'unaryOperation', label: 'cosh', id: 'cosh' }
+export const tangentH = { type: 'unaryOperation', label: 'tanh', id: 'tanh' }
+
+/* Constant Keys
+-----------------------------------------------*/
+export const e = { type: 'constant', label: 'e', id: 'E' }
+export const pi = { type: 'constant', label: '&Pi;', id: 'PI' }
+export const rand = { type: 'constant', label: 'Rand', id: 'random' }
