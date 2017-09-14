1e573bd394bde8d407a009435056e5b0302c1d25
lukePeavey <1williampeavey@gmail.com>
2017-09-14 19:36:07 -0400

    Refactor Keypad component

--- src/components/keypad/Keypad.js
+++ src/components/keypad/Keypad.js
@@ -6,20 +6,15 @@ const Keypad = ({ keys: rows, handleClick, currentOperation, trigUnit }) => (
   <section className="Keypad">
     {rows.map((keys, index) => (
       <div className="Keypad-row" key={`row${index}`}>
-        {keys.map(({ id, ...keyProps }) => {
-          // @todo - Rename keys and keypad something else.
-          // Just noticed this is very confusing with React's key prop.
-          return (
-            <Key
-              key={id}
-              id={id}
-              handleClick={handleClick}
-              active={currentOperation === id}
-              trigUnit={trigUnit}
-              {...keyProps}
-            />
-          )
-        })}
+        {keys.map(keyProps => (
+          <Key
+            key={keyProps.id}
+            handleClick={handleClick}
+            active={currentOperation === keyProps.id}
+            trigUnit={trigUnit}
+            {...keyProps}
+          />
+        ))}
       </div>
     ))}
   </section>
